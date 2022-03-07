34b06f63da03e87807fb2a7f525b99ff4251ba84
HeitorKun <60748906+HeitorKun@users.noreply.github.com>
2022-03-07 16:54:52 -0300

    fixing api call

--- app dos carros/app dos carros/CarService/CarService.swift
+++ app dos carros/app dos carros/CarService/CarService.swift
@@ -13,32 +13,38 @@ class CarService {
     func login(username: String, password: String,  completion: @escaping ([LoginSuccessPostReturn]?) -> ()) {
 
         let url = URL(string: loginPostAPIAdress)
-        guard let requestUrl = url else { fatalError() }
+        guard let requestUrl = url else { completion(nil)
+            return }
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
 
-        // HTTP Request Parameters which will be sent in HTTP Request Body
-        let postString = "username="+username+"&password="+password
-        // Set HTTP Request Body
-        request.httpBody = postString.data(using: String.Encoding.utf8);
-        // Perform HTTP Request
+        let body = ["username": username,
+                    "password": password]
+        let bodyData = try? JSONSerialization.data(
+            withJSONObject: body,
+            options: []
+        )
+        request.httpBody = bodyData
+
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
 
-                // Check for Error
-                if let error = error {
-                    print("Error took place \(error)")
-                    return
-                }
+            // Check for Error
+            if error != nil {
+                completion(nil)
+                return
+            }
 
-                // Convert HTTP Response Data to a String
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("Response data string:\n \(dataString)")
-                }
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
         }
         task.resume()
 
     }
+
+}
 
 
 }
