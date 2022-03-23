06f8a688f4bf44873be637115bb34efabaa930a1
HeitorKun <60748906+HeitorKun@users.noreply.github.com>
2022-03-23 18:38:29 -0300

    just separated a function that fetches a image from url  to another file

---
+++ app dos carros/app dos carros/Extentions/UIImageViewExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIImageViewExtension.swift
+//  app dos carros
+//
+//  Created by Heitor Feijó Kunrath on 23/03/22.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIImageView {
+    func loadFrom(URLAddress: String) {
+        guard let url = URL(string: URLAddress) else {
+            return
+        }
+
+        DispatchQueue.main.async { [weak self] in
+            if let imageData = try? Data(contentsOf: url) {
+                if let loadedImage = UIImage(data: imageData) {
+                        self?.image = loadedImage
+                }
+            }
+        }
+    }
+}
